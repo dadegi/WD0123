8462675677793c3e3eec317e74a96b3fe84bb1a8
Naviganet <dadegi@gmail.com>
2023-02-09 11:18:50 +0100

    Lezione Javascript settimana 2 giorno 4

---
+++ week2/G4/LezioneG4/assets/js/script.js
@@ -0,0 +1,55 @@
+var btnCalcola = document.getElementById('calcola');
+var btnReset = document.getElementById('reset');
+var operazione;
+var numero1;
+var numero2;
+var risultato;
+
+btnCalcola.addEventListener('click', function () {
+	operazione = document.getElementById('operazione').value;
+	numero1 = document.getElementById('numero1').value;
+	numero2 = document.getElementById('numero2').value;
+	if (!controlla()) {
+		return;
+	}
+	calcola();
+    scrivi();
+});
+
+function controlla() {
+	if (operazione == '' || numero1 == '' || numero2 == '') {
+		document.getElementById('risultato').innerHTML =
+			"Attenzione! Compilare tutti i campi e scegliere un'operazione!";
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function calcola() {
+	switch (operazione) {
+		case 'addizione':
+			risultato = numero1 + numero2;
+			break;
+		case 'sottrazione':
+			risultato = numero1 - numero2;
+			break;
+		case 'moltiplicazione':
+			risultato = numero1 * numero2;
+			break;
+		case 'divisione':
+			risultato = numero1 / numero2;
+			break;
+		default:
+			risultato = 0;
+			break;
+	}
+}
+
+function scrivi() {
+    document.getElementById('risultato').innerHTML = `Il risultato della ${operazione} tra ${numero1} e ${numero2} è ${risultato}`;
+}
+
+btnReset.addEventListener('click', function() {
+    document.getElementById('risultato').innerHTML = '';
+});
